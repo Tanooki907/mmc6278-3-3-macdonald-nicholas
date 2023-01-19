29055297c9016fd4ed3f528d405b758803923343
Nicholas MacDonald <macdonaldn@ufl.edu>
2023-01-19 16:50:55 -0500

    build relocate app

--- app.js
+++ app.js
@@ -2,10 +2,55 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 // TODO: import the getCityInfo and getJobs functions from util.js
+app.use(express.json())
+const util = require('./util')
+var cityInfo
+var jobs
 
+let getCityInfo = async() => {
+    cityInfo = await util.getCityInfo;
+}
+let getJobs = async() => {
+    jobs = await util.getJobs;
+}
 // TODO: Statically serve the public folder
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.js'))
+})
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/style.css'))
+})
+
 // TODO: declare the GET route /api/city/:city
+app.get('/api/city/:city', async (req, res) => {
+    try {
+        //let [cityInfo, jobs] = await Promise.all([util.getCityInfo(req), util.getJobs(req)])
+        let cityInfo = await util.getCityInfo(req)
+        let jobs = await util.getJobs(req)
+        if (!cityInfo && !jobs){
+            throw new Error
+        }
+        res.json({
+            cityInfo,
+            jobs
+        })
+        console.log(res.json)
+    }catch (err){
+        res.status(404).json({
+            error: err
+        })
+        return res.status(404).end()
+    }
+    }
+)
 // This endpoint should call getCityInfo and getJobs and return
 // the result as JSON.
 // The returned JSON object should have two keys:
